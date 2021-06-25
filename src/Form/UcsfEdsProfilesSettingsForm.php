0619a5690cb8c5b88eccb417daac7a8dae30f421
James Huang <jhu9000@gmail.com>
2021-06-24 22:57:35 -0700

    meep

--- src/Form/UcsfEdsProfilesSettingsForm.php
+++ src/Form/UcsfEdsProfilesSettingsForm.php
@@ -42,6 +42,14 @@ class UcsfEdsProfilesSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config(static::SETTINGS);
 
+    // $form['email_fn'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Email field machine name'),
+    //   '#default_value' => $config->get('email_fn'),
+    //   '#description' => $this->t('The email field on ucsf_eds_profiles content type is required for EDS ldap search.'),
+    //   '#required' => TRUE,
+    // ];
+
     $form['ldap_mn'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Ldap server machine name'),
@@ -50,14 +58,26 @@ class UcsfEdsProfilesSettingsForm extends ConfigFormBase {
       '#required' => TRUE,
     ];
 
-    $form['email_fn'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Email field machine name'),
-      '#default_value' => $config->get('email_fn'),
-      '#description' => $this->t('The email field on ucsf_eds_profiles content type is required for EDS ldap search.'),
+    $form['cron_delta'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Cron delta'),
+      '#default_value' => $config->get('cron_delta'),
+      '#description' => $this->t('Minimum increment of time in seconds between cron sync of EDS/Profiles nodes with source.'),
+      '#min' => 0,
       '#required' => TRUE,
     ];
 
+    for($i=1;$i<10;$i++) {
+      $options[$i] = $i;
+    }
+    $form['allowed_release_codes'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Allowed release codes'),
+      '#options' => $options,
+      '#default_value' => $config->get('allowed_release_codes'),
+      '#description' => $this->t('Select the release codes that are allowed to store data.<br>UCSF levels of release indicates the protection level of the corresponding data for a person.<br>See https://wiki.library.ucsf.edu/display/IAM/Level+of+Release for release code descriptions.'),
+      '#required' => TRUE,
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -66,8 +86,10 @@ class UcsfEdsProfilesSettingsForm extends ConfigFormBase {
    */
   public function settingsKeys() {
     return [
-      'email_fn',
+      // 'email_fn',
       'ldap_mn',
+      'cron_delta',
+      'allowed_release_codes',
     ];
   }
 
